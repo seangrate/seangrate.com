a5c678056bf18118c6a80e48650a8e7aa7932c0f
Sean <thegratesalmon@gmail.com>
2020-07-13 23:07:00 -0400

    Added base.html as a template and adjusted other HTML files accordingly.

--- application.py
+++ application.py
@@ -4,27 +4,39 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html', title='Sean Grate',
+                           is_home=True,
+                           nav_buttons=['Research', 'Projects'])
 
 @app.route('/assignments')
 def assignments():
-    return render_template("assignments.html")
+    return render_template('assignments.html', title='TA Assignments',
+                           is_home=False,
+                           nav_buttons=['Background', 'Algorithm'])
 
 @app.route('/flightroute')
 def flightroute():
-    return render_template("flightroute.html")
+    return render_template('flightroute.html', title='Estimating Flight Routes',
+                           is_home=False,
+                           nav_buttons=['Background', 'Approaches'])
 
 @app.route('/heisenberg')
 def heisenberg():
-    return render_template("heisenberg.html")
+    return render_template('heisenberg.html', title='Discrete Heisenberg Group',
+                           is_home=False,
+                           nav_buttons=['Background', 'Setup', 'Results'])
 
 @app.route('/minecraft')
 def minecraft():
-    return render_template("minecraft.html")
+    return render_template('minecraft.html', title='Generating Minecraft Worlds',
+                           is_home=False,
+                           nav_buttons=['Background', 'Approach'])
 
 @app.route('/visualizations')
 def visualizations():
-    return render_template("visualizations.html")
+    return render_template('visualizations.html', title='Visualizing Algebraic Surfaces',
+                           is_home=False,
+                           nav_buttons=['27 Lines', 'Calculus III'])
 
 
 if __name__ == '__main__':
