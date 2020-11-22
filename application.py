79ff978c4fe5e90d63da5a2e0ed041473f0e95d4
Sean <thegratesalmon@gmail.com>
2020-11-21 22:27:46 -0600

    Updated some file paths.

--- application.py
+++ application.py
@@ -1,48 +1,48 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html', title='Sean Grate',
                            is_home=True,
                            nav_buttons=['About', 'Research', 'Projects'])
 
-@app.route('/assignments')
+@application.route('/assignments')
 def assignments():
     return render_template('assignments.html', title='TA Assignments',
                            is_home=False,
                            nav_buttons=['Background', 'Algorithm'])
 
-@app.route('/flightroute')
+@application.route('/flightroute')
 def flightroute():
     return render_template('flightroute.html', title='Estimating Flight Routes',
                            is_home=False,
                            nav_buttons=['Background', 'Approaches'])
 
-@app.route('/heisenberg')
+@application.route('/heisenberg')
 def heisenberg():
     return render_template('heisenberg.html', title='Discrete Heisenberg Group',
                            is_home=False,
                            nav_buttons=['Background', 'Setup', 'Results'])
 
-@app.route('/minecraft')
+@application.route('/minecraft')
 def minecraft():
     return render_template('minecraft.html', title='Generating Minecraft Worlds',
                            is_home=False,
                            nav_buttons=['Background', 'Approach'])
 
-@app.route('/visualizations')
+@application.route('/visualizations')
 def visualizations():
     return render_template('visualizations.html', title='Visualizing Algebraic Surfaces',
                            is_home=False,
                            nav_buttons=['27 Lines', 'Twisted Cubic', 'Calculus III'])
 
 
-@app.route('/test')
+@application.route('/test')
 def test():
     return render_template('test.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
